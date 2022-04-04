e58a87e1ecf52d621b5ad42b10f0eb42af311022
dknsapatin <dknsapatin@gmail.com>
2022-04-03 22:06:49 -0400

    Created and finished Product page

---
+++ src/pages/Product.jsx
@@ -0,0 +1,156 @@
+import styled from "styled-components";
+import Navbar from "../components/Navbar";
+import Announcement from "../components/Announcement";
+import Footer from "../components/Footer";
+import Newsletter from "../components/Newsletter";
+import { Add, Remove } from "@material-ui/icons";
+
+const Container = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  padding: 50px;
+`;
+const ImgContainer = styled.div`
+  flex: 1;
+`;
+const Image = styled.img`
+  width: 100%;
+  height: 90vh;
+  object-fit: cover;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 0px 50px;
+`;
+const Title = styled.div`
+  font-size: 50px;
+  font-weight: 100;
+`;
+const Description = styled.p`
+  margin: 20px 0px;
+  font-size: 25px;
+  font-weight: 100;
+`;
+const Price = styled.span`
+  font-weight: 100px;
+  font-size: 40px;
+`;
+
+const FilterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 50%;
+  margin: 30px 0px;
+`;
+const Filter = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const FilterTitle = styled.div`
+  font-style: 20px;
+  font-weight: 200;
+`;
+const FilterColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+  margin: 0px 5px;
+  cursor: pointer;
+`;
+const FilterSize = styled.select`
+  margin-left: 20px;
+  padding: 5px;
+`;
+const FilterSizeOption = styled.option``;
+
+const AddContainer = styled.div`
+  width: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+const AmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  font-weight: 700;
+`;
+const Amount = styled.span`
+  width: 30px;
+  height: 30px;
+  border-radius: 10px;
+  border: 1px solid teal;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0px 5px;
+`;
+const Button = styled.button`
+  padding: 15px;
+  border: 1px solid teal;
+  background-color: white;
+  cursor: pointer;
+  font-weight: 500;
+
+  &:hover {
+    background-color: #f8f4f4;
+  }
+`;
+
+const Product = () => {
+  return (
+    <Container>
+      <Navbar />
+      <Announcement />
+      {/* This wrapper container is specific for each Product */}
+      <Wrapper>
+        <ImgContainer>
+          <Image src="https://i.ibb.co/S6qMxwr/jean.jpg" />
+        </ImgContainer>
+        <InfoContainer>
+          <Title>Denim Jumpsuit</Title>
+          <Description>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
+            exercitationem ab perferendis ad, eos quas officiis natus quo dolor
+            impedit officia nisi quibusdam numquam eligendi necessitatibus,
+            quidem accusamus suscipit iure?
+          </Description>
+          <Price>$20</Price>
+          <FilterContainer>
+            <Filter>
+              <FilterTitle>Color</FilterTitle>
+              <FilterColor color="black" />
+              <FilterColor color="darkblue" />
+              <FilterColor color="gray" />
+            </Filter>
+
+            <Filter>
+              <FilterTitle>Size</FilterTitle>
+              <FilterSize>
+                <FilterSizeOption>XS</FilterSizeOption>
+                <FilterSizeOption>S</FilterSizeOption>
+                <FilterSizeOption>M</FilterSizeOption>
+                <FilterSizeOption>L</FilterSizeOption>
+                <FilterSizeOption>XL</FilterSizeOption>
+              </FilterSize>
+            </Filter>
+          </FilterContainer>
+
+          <AddContainer>
+            <AmountContainer>
+              <Remove />
+              <Amount>1</Amount>
+              <Add />
+            </AmountContainer>
+            <Button>ADD TO CART</Button>
+          </AddContainer>
+        </InfoContainer>
+      </Wrapper>
+      <Newsletter />
+      <Footer />
+    </Container>
+  );
+};
+
+export default Product;
