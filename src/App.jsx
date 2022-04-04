73c2be914406019870e9ce9d8b3e6618faaa4e55
Denzel Sapatin <dknsapatin@gmail.com>
2022-04-04 14:45:34 -0400

    Finished Cart Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 // import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
 const App = () => {
-  return <Login />;
+  return <Home />;
 };
 
 export default App;
